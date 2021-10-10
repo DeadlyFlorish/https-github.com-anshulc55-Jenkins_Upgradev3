91df20b6c09433e908c70ed9479f43b6d57f72dc
DeadlyFlorish <benfajria.khaled11@gmail.com>
2021-10-10 13:41:38 +0100

    change khaled2

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -39,7 +39,7 @@ public class TestGreeter {
   }
 
   @Test
-public void greetShouldIncludeGreetingMessage() {
+public void greetShouldIncludeGreetingMessage2() {
   String someone = "World, Thanks you khaled";
 
   assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
