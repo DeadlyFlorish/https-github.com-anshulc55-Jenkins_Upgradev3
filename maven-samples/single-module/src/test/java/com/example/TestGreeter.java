d999cb02dc9d82a4578fb37c50d51297ee12ad47
Khaled, Benfajria <benfajria.khaled11@gmail.com>
2022-03-05 19:20:47 +0000

    maven-pfe

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -45,7 +45,7 @@ public void greetShouldIncludeGreetingMessage2() {
   assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
 }
   @Test
-public void greetShouldIncludeGreetingMessage2() {
+public void greetShouldIncludeGreetingMessage3() {
   String someone = "World, Thanks you khaled PFE";
 
   assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
